7177ae2d44c0b51582238a778b93dd1847f4799e
Monster dragon <165640633+Sumitmeena12@users.noreply.github.com>
2024-05-01 16:22:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @ashif903 on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6601119415))
+OWNER_ID = int(getenv("OWNER_ID", 6250035118))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -34,7 +34,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/RADHEEXUSER/RADHE-X-MUSIC-",
+    "https://github.com/Sumitmeena12/RADHE-X-MUSIC-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
@@ -42,7 +42,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/dragondpworld")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+2i2Fm7uVG-w4YTBl")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/apnihavelichattingxd")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
